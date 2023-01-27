a8bbc76dbf970c3085c3147add16df61e61ab396
fishermanaustin <austinp0302@gmail.com>
2023-01-26 16:18:47 -0800

    Update Pragma.h

--- include/Pragma.h
+++ include/Pragma.h
@@ -3,17 +3,16 @@
 #define _PROS_PRAGMA_H_
 #include "api.h"
 pros::Controller master(pros::E_CONTROLLER_MASTER);
-pros::ADIDigitalOut expansion('C', false);
+pros::ADIDigitalOut expansion('B', false);
 pros::ADIDigitalIn limswitch('A');
-pros::Motor left_frt(16, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor left_bck(3, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor left_mid(15, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor right_frt(13, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor right_bck(2, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor right_mid(17, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor intake_1(1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Motor cata(21, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_ROTATIONS);
-//pros::Motor intake_2(7, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_ROTATIONS);
-pros::Imu gyro1(18);
+pros::Motor left_frt(1, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor left_bck(2, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor left_mid(3, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor right_frt(4, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor right_bck(5, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor right_mid(6, pros::E_MOTOR_GEARSET_06, true, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor intake_1(7, pros::E_MOTOR_GEARSET_06, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Motor cata(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_ROTATIONS);
+pros::Imu gyro1(9);
 pros::Imu gyro2(10);
 #endif 
